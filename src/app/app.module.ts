02ecc71e826f48ed453b1a2c26dea1c744264738
azeezshaik2 <shaik.azeez2@gmail.com>
2019-04-06 18:15:38 +0530

    commit first changes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { SampleMaterialSliderComponent } from './components/sample-material-slider/sample-material-slider.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SampleMaterialSliderComponent
   ],
   imports: [
     BrowserModule,
